06b150a79089edfbe74e69f940cfc6ef5f930306
Marcelo Knauer <marceloknauer.dev@gmail.com>
2025-12-09 17:15:14 -0300

    Reverse list

--- Generics/ReverseGenericList/Main.java
+++ Generics/ReverseGenericList/Main.java
@@ -1,7 +1,13 @@
 package Desafios.Generics.ReverseGenericList;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static Desafios.Generics.ReverseGenericList.Services.ReverseList.*;
+
 public class Main {
     public static void main(String[] args) {
-
+        List<Integer> lst = new ArrayList<Integer>(List.of(1,2,3,4,5,6));
+        System.out.println("Reverse list: "+reverseList(lst));
     }
 }
--- Generics/ReverseGenericList/Services/ReverseList.java
+++ Generics/ReverseGenericList/Services/ReverseList.java
@@ -1,4 +1,16 @@
 package Desafios.Generics.ReverseGenericList.Services;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ReverseList {
+    public static <T> List<T> reverseList(List<T> lst) {
+        List<T> reversedList = new ArrayList<>();
+
+        for (int i = lst.size() - 1; i >= 0; i--) {
+            reversedList.add(lst.get(i));
+        }
+
+        return reversedList;
+    }
 }
