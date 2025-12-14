961d4fce656baed786b4fba269713a93be1685b8
Marcelo Knauer <marceloknauer.dev@gmail.com>
2025-12-14 15:11:33 -0300

    Rectangle
    Write a Java program to create a class called "Rectangle" with width and height attributes. Calculate the area and perimeter of the rectangle.

---
+++ OOP/Rectangles/Main.java
@@ -0,0 +1,11 @@
+package Desafios.POO.Rectangles;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Rectangle rectangle = new Rectangle(10, 5);
+
+        System.out.println("Area: "+ rectangle.getArea());
+        System.out.println("Perimeter: "+ rectangle.getPerimeter());
+    }
+}
