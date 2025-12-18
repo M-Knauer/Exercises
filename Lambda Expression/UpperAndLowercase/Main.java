68c4106033b7ac4a916b4391467b94372293d747
Marcelo Knauer <marceloknauer.dev@gmail.com>
2025-12-18 15:11:30 -0300

    Title case
    Write a Java program to implement a lambda expression that converts a list of strings to title case.

--- Lambda Expression/UpperAndLowercase/Main.java
+++ Lambda Expression/UpperAndLowercase/Main.java
@@ -5,13 +5,22 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<String> str = new ArrayList<String>(List.of("Muscle", "Wits", "Dexterity", "Intelligence"));
+        List<String> str = new ArrayList<String>(List.of("Muscle", "wits", "DEXTERITY", "InteLliGencE"));
 
         str.replaceAll(s -> s.toUpperCase());
         System.out.println("Uppercase: "+str);
 
         str.replaceAll(s -> s.toLowerCase());
         System.out.println("Lowercase: "+str);
+
+        str.replaceAll(s -> {
+            if (s != null && !s.trim().isEmpty()){
+                s = s.trim();
+                s = Character.toTitleCase(s.charAt(0)) + s.substring(1);
+            }
+            return s;
+        });
+        System.out.println("Title case: "+str);
 
     }
 }
