a9b4b9043b0f0fd5f929663a2ab87bd951b6060e
Marcelo Knauer <marceloknauer.dev@gmail.com>
2025-12-20 19:26:01 -0300

    Sort by length
    Write a Java program to create a lambda that sorts a list of strings by length and then alphabetically for equal lengths.

--- Lambda Expression/AlphabeticalOrder/Main.java
+++ Lambda Expression/AlphabeticalOrder/Main.java
@@ -1,6 +1,7 @@
 package Desafios.LambdaExpression.AlphabeticalOrder;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class Main {
@@ -12,6 +13,16 @@ public class Main {
 
         colors.sort((c1, c2) -> c2.compareToIgnoreCase(c1));
         System.out.println("Reverse alphabetical order: "+colors);
+
+        Comparator<String> lengthOrder = (s1, s2) -> {
+            return Integer.compare(s1.length(), s2.length());
+        };
+
+        Comparator<String> ifEqualsLenAlpha = (s1, s2) -> {
+            return s1.compareToIgnoreCase(s2);
+        };
+        colors.sort(lengthOrder.thenComparing(ifEqualsLenAlpha));
+        System.out.println("Sort by length: "+colors);
 
     }
 }
