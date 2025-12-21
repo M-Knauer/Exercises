cc7559149f9d5144df98398447aa786c1f68fc99
Marcelo Knauer <marceloknauer.dev@gmail.com>
2025-12-21 01:41:32 -0300

    Ignoring negative values
    Write a Java program to implement a lambda expression that calculates the average of a list of doubles while ignoring negative values.

--- Lambda Expression/Average/Main.java
+++ Lambda Expression/Average/Main.java
@@ -5,9 +5,16 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<Double> numbers = new ArrayList<Double>(List.of(3.5, 7.5, 4.3, 4.7, 5.1));
+        List<Double> numbers = new ArrayList<Double>(List.of(3.5, 7.5, 4.3, 4.7, 5.1, -3.0, -2.1));
 
         double average = numbers.stream().mapToDouble(d -> d).average().orElse(0.);
         System.out.println("Average: "+String.format("%.2f", average));
+
+        double ignoreNegativesAvr = numbers.stream()
+                .filter(d -> d > 0)
+                .mapToDouble(d -> d)
+                .average().orElse(0.);
+
+        System.out.println("Average Ignoring negative values: "+String.format("%.2f", ignoreNegativesAvr));
     }
 }
