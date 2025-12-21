79929841fc5025fca10c19e8f6f20d345fbb47db
Marcelo Knauer <marceloknauer.dev@gmail.com>
2025-12-21 17:44:31 -0300

    Remove duplicate values
    Write a Java program to implement a lambda expression to remove duplicates from a list of integers.

---
+++ Lambda Expression/Duplicate/Main.java
@@ -0,0 +1,16 @@
+package Desafios.LambdaExpression.Duplicate;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> numbers = new ArrayList<>(List.of(21, 1, 2, 3, 3, 4, 3, 0, 0, 2, 11, 5, 6, 1, 7, 7, 8, 10));
+        List<Integer> uniques = new ArrayList<>();
+
+        uniques.addAll(numbers.stream()
+                .distinct().toList());
+
+        System.out.println("Unique numbers: "+uniques);
+    }
+}
