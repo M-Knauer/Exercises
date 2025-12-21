92b860bee5bddf5f068cc3875b2d15116fa99ae3
Marcelo Knauer <marceloknauer.dev@gmail.com>
2025-12-21 15:06:54 -0300

    Parallel stream vs normal stream
    Write a Java program to implement a lambda that computes the average of a double stream in parallel and compares it with sequential computation.

--- Lambda Expression/Average/Main.java
+++ Lambda Expression/Average/Main.java
@@ -7,20 +7,45 @@ public class Main {
     public static void main(String[] args) {
         List<Double> numbers = new ArrayList<Double>(List.of(3.5, 7.5, 4.3, 4.7, 5.1, -3.0, -2.1));
 
-        double average = numbers.stream().mapToDouble(d -> d).average().orElse(0.);
-        System.out.println("Average: "+String.format("%.2f", average));
+        double average = numbers.stream()
+                .mapToDouble(d -> d)
+                .average().orElse(0.);
+
+        System.out.println("Average: "+average);
 
         double ignoreNegativesAvr = numbers.stream()
                 .filter(d -> d > 0)
                 .mapToDouble(d -> d)
                 .average().orElse(0.);
 
-        System.out.println("Average Ignoring negative values: "+String.format("%.2f", ignoreNegativesAvr));
+        System.out.println("Average Ignoring negative values: "+ignoreNegativesAvr);
 
         double averageSquare = numbers.stream()
                 .mapToDouble(d -> d * d)
                 .average().orElse(0.0);
 
         System.out.println("Average square: "+averageSquare);
+
+        System.out.println();
+
+        long start = System.nanoTime();
+        double parallelAverage = numbers.parallelStream()
+                .mapToDouble(d -> d)
+                .average().orElse(0.0);
+
+        System.out.println("Parallel average: "+parallelAverage);
+        long end = System.nanoTime();
+        System.out.println("Parallel stream time: "+(end - start));
+
+        System.out.println();
+
+        start = System.nanoTime();
+        double normalAverage = numbers.stream()
+                .mapToDouble(Double::doubleValue)
+                .average().orElse(0.0);
+
+        System.out.println("Normal average: "+normalAverage);
+        end = System.nanoTime();
+        System.out.println("Normal stream time: "+(end - start));
     }
 }
