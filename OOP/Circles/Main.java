13464c5c98477457df284de370deba6c1fb6578e
Marcelo Knauer <marceloknauer.dev@gmail.com>
2025-12-22 23:21:03 -0300

    Cricle
     Write a Java program to create a class called "Circle" with a radius attribute. You can access and modify this attribute. Calculate the area and circumference of the circle.

---
+++ OOP/Circles/Main.java
@@ -0,0 +1,11 @@
+package Desafios.POO.Circles;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Circle circle = new Circle(10);
+
+        System.out.println("Area: "+ circle.getArea());
+        System.out.println("Circumference: "+ circle.getCircumference());
+    }
+}
