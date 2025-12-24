b758d2593485cbb2b1198c287ab08c65c2352520
Marcelo Knauer <marceloknauer.dev@gmail.com>
2025-12-24 18:44:33 -0300

    IntStream
    Write a Java program to implement a lambda expression that finds all prime numbers in a given range and collects them into a list.

--- Lambda Expression/Prime/Main.java
+++ Lambda Expression/Prime/Main.java
@@ -2,6 +2,7 @@ package Desafios.LambdaExpression.Prime;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.IntPredicate;
 import java.util.function.Predicate;
 import java.util.stream.IntStream;
 
@@ -24,6 +25,22 @@ public class Main {
 
         List<Integer> numbers = new ArrayList<>(List.of(1,2,3,4,5,6,7,8,9,10));
         List<Integer> primes = numbers.stream().filter(isPrime).toList();
-        System.out.println(primes);
+        System.out.println("Primes: "+primes);
+
+        int start = 1;
+        int end = 37;
+        IntPredicate isPrimeInt = n -> {
+            if (n <= 1) {
+                return false;
+            }
+            for (int i = 2; i <= Math.sqrt(n); i++) {
+                if (n % i == 0) {
+                    return false;
+                }
+            }
+            return true;
+        };
+        primes = IntStream.rangeClosed(start, end).filter(isPrimeInt).boxed().toList();
+        System.out.println("Primes in range "+start+" to "+end+": "+primes);
     }
 }
