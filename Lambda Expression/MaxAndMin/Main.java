6ad7fae65b1c05f8c137b80ca613d527e3a59aa2
Marcelo Knauer <marceloknauer.dev@gmail.com>
2025-12-26 18:08:11 -0300

    Calculates the difference
    Write a Java program to create a lambda that calculates the difference between the maximum and minimum values from a list of integers.

--- Lambda Expression/MaxAndMin/Main.java
+++ Lambda Expression/MaxAndMin/Main.java
@@ -3,10 +3,11 @@ package Desafios.LambdaExpression.MaxAndMin;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.function.Function;
 
 public class Main {
     public static void main(String[] args) {
-        List<Integer> numbers = new ArrayList<>(List.of(12, 15, 0, 8, 7, 9, -6));
+        List<Integer> numbers = new ArrayList<>(List.of());
 
         System.out.println("Max and min numbers");
         Optional<Integer> max = numbers.stream().max(Integer::compareTo);
@@ -20,6 +21,13 @@ public class Main {
         System.out.println("Max: "+maxInt);
         System.out.println("Min: "+minInt);
 
+        System.out.println("\nLambda to calculate the difference between max and mix numbers");
+        Function<List<Integer>, Integer> calculateDifference = integers -> {
+            Optional<Integer> maxTmp = integers.stream().max(Integer::compareTo);
+            Optional<Integer> minTmp = integers.stream().min(Integer::compareTo);
 
+            return maxTmp.orElse(0) - minTmp.orElse(0);
+        };
+        System.out.println("Max and min difference: "+calculateDifference.apply(numbers));
     }
 }
