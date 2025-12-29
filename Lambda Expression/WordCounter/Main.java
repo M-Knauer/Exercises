c6b6a3d07903ed69e64899c6496a26234c0943d2
Marcelo Knauer <marceloknauer.dev@gmail.com>
2025-12-29 19:39:43 -0300

    Filters out stop words
    Write a Java program to create a lambda that splits a sentence into words and filters out stop words before counting.

--- Lambda Expression/WordCounter/Main.java
+++ Lambda Expression/WordCounter/Main.java
@@ -2,15 +2,28 @@ package Desafios.LambdaExpression.WordCounter;
 
 import Desafios.LambdaExpression.WordCounter.Models.WordCounter;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
 public class Main {
     public static void main(String[] args) {
-        String sentence = "  Feliz ano novo para, todos !";
+        String sentence = "Olá, mundo e feliz natal!";
         System.out.println("Sentence: "+sentence);
         WordCounter wordCounter = s -> s.trim().split("\\s+").length;
         System.out.println("Word count: "+wordCounter.count(sentence));
 
         System.out.println("\nRemoving string punctuation");
         wordCounter = s -> s.trim().replaceAll("[,.;!?:]", "").split("\\s+").length;
-        System.out.println(wordCounter.count(sentence));
+        System.out.println(sentence.trim().replaceAll("[,.;!?:]", ""));
+        System.out.println("Word count: "+wordCounter.count(sentence));
+
+        System.out.println("\nRemoving stop words");
+        Predicate<String> filterStopWords = text ->
+                !List.of("o", "a", "de", "e", "para", "em").contains(text.toLowerCase());
+        wordCounter = s -> Arrays.stream(s.trim().split("\\s+")).filter(filterStopWords).toArray().length;
+        Arrays.stream(sentence.trim().split("\\s+")).filter(filterStopWords).forEach(s ->
+                System.out.print(s+" "));
+        System.out.println("\nWord count: "+wordCounter.count(sentence));
     }
 }
