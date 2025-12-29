8f314cb7ec1f828caf2e3c26982e02a5c1eaf22e
Marcelo Knauer <marceloknauer.dev@gmail.com>
2025-12-29 00:04:26 -0300

    Sum of products for pairs of adjacent elements
    Write a Java program to chain lambda expressions that compute the sum of products for pairs of adjacent elements in a list.

--- Lambda Expression/MultiplyAndSum/Main.java
+++ Lambda Expression/MultiplyAndSum/Main.java
@@ -1,11 +1,11 @@
 package Desafios.LambdaExpression.MultiplyAndSum;
 
 import Desafios.LambdaExpression.MultiplyAndSum.Models.Result;
-import Desafios.POO.MusicLibraries.Song;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Function;
+import java.util.stream.IntStream;
 
 public class Main {
     public static void main(String[] args) {
@@ -38,5 +38,34 @@ public class Main {
         Result resObj = multipliesSum.apply(numbers);
         System.out.println(resObj.multiply);
         System.out.println(resObj.sum);
+
+        System.out.println("\nSum of products for pairs of adjacent elements");
+        Function<List<Integer>, List<Integer>> multipliesPairs = integers -> {
+            List<Integer> multipliedPairs = new ArrayList<>();
+
+            int index1 = 0;
+            int index2 = 1;
+            int limit = integers.size() - 1;
+            while (index2 <= limit) {
+                int n1 = integers.get(index1);
+                int n2 = integers.get(index2);
+                multipliedPairs.add(n1 * n2);
+                index1++;
+                index2++;
+            }
+
+            return multipliedPairs;
+        };
+
+        Function<List<Integer>, Integer> sumPairs = integers ->
+                integers.stream().mapToInt(Integer::intValue).sum();
+
+        System.out.println(multipliesPairs.andThen(sumPairs).apply(numbers));
+
+        // alternative way
+        int total = IntStream.rangeClosed(0, numbers.size() - 2)
+                .map(i -> numbers.get(i) * numbers.get(i+1))
+                .sum();
+        System.out.println(total);
     }
 }
