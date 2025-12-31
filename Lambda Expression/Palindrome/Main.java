87e22756527c004295d36be7d857a2fcb88770f6
Marcelo Knauer <marceloknauer.dev@gmail.com>
2025-12-31 18:48:19 -0300

    Regex
    Write a Java program to create a lambda that checks palindromicity of a string ignoring case and non-alphanumeric characters.

--- Lambda Expression/Palindrome/Main.java
+++ Lambda Expression/Palindrome/Main.java
@@ -4,16 +4,22 @@ import java.util.function.Predicate;
 
 public class Main {
     public static void main(String[] args) {
-        String text = "A grama é amarga";
+        String text = "ana";
 
-        Predicate<String> isPalindrome = s -> {
-            String formatedS = s.toLowerCase().replace(" ","");
-            StringBuilder reversedS = new StringBuilder(formatedS);
-            reversedS.reverse();
-            return formatedS.contentEquals(reversedS);
-        };
+        Predicate<String> isPalindrome = s -> s.contentEquals(new StringBuilder(s).reverse());
 
         System.out.println(isPalindrome.test(text) ? text+" is a palindrome" : text+" isn't a palindrome");
+
+        System.out.println("\nIgnoring case and non-alphanumeric characters");
+        isPalindrome = s -> {
+            String formatedS = s.replaceAll("\\W", "");
+            StringBuilder reversedS = new StringBuilder(formatedS);
+            reversedS.reverse();
+            return formatedS.equalsIgnoreCase(reversedS.toString());
+        };
+        text = "A Daniela ama a lei? Nada!";
+        System.out.println(isPalindrome.test(text) ? "'"+text+"' is a palindrome" : "'"+text+"' isn't a palindrome");
+
 
     }
 }
