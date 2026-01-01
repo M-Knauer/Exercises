8df5ad811c0cfd6ee2c767f21e75a5bf9745c010
Marcelo Knauer <marceloknauer.dev@gmail.com>
2025-12-31 22:19:07 -0300

    Filter out palindromic strings
    Write a Java program to chain lambda expressions to filter out palindromic strings from a list.

--- Lambda Expression/Palindrome/Main.java
+++ Lambda Expression/Palindrome/Main.java
@@ -1,5 +1,7 @@
 package Desafios.LambdaExpression.Palindrome;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.Predicate;
 
 public class Main {
@@ -11,15 +13,35 @@ public class Main {
         System.out.println(isPalindrome.test(text) ? text+" is a palindrome" : text+" isn't a palindrome");
 
         System.out.println("\nIgnoring case and non-alphanumeric characters");
-        isPalindrome = s -> {
+        Predicate<String> isPalindromeIgnoringCase = s -> {
             String formatedS = s.replaceAll("\\W", "");
             StringBuilder reversedS = new StringBuilder(formatedS);
             reversedS.reverse();
             return formatedS.equalsIgnoreCase(reversedS.toString());
         };
         text = "A Daniela ama a lei? Nada!";
-        System.out.println(isPalindrome.test(text) ? "'"+text+"' is a palindrome" : "'"+text+"' isn't a palindrome");
+        System.out.println(isPalindromeIgnoringCase.test(text) ?
+                "'"+text+"' is a palindrome" : "'"+text+"' isn't a palindrome");
 
+        System.out.println("\nCheck if every word in a sentence is a palindrome");
+        Predicate<String> isAllPalindrome = s -> {
+            List<String> txtList = new ArrayList<>(List.of(s.split("\\s+")));
+            for (String str : txtList) {
+                if (!str.contentEquals(new StringBuilder(str).reverse())) {
+                    return false;
+                }
+            }
+            return true;
+        };
+        text = "ana ama ovo";
+        System.out.println(isAllPalindrome.test(text) ?
+                "Every word in: '"+text+"' is a palindrome" :
+                "There is non-palindrome word in: '"+text+"'");
 
+        System.out.println("\nFilter out palindromic strings from a list");
+        List<String> texts = new ArrayList<>(List.of("ana", "lei", "osso", "nada"));
+        Predicate<String> filterOutPalindrome = s -> !s.contentEquals(new StringBuilder(s).reverse());
+        List<String> nonPalindrome = texts.stream().filter(filterOutPalindrome).toList();
+        System.out.println("Filtered list: "+nonPalindrome);
     }
 }
