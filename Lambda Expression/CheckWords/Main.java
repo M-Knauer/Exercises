e64d3b91837d75f4fa43c5bc6c3c59d8f0c3a457
Marcelo Knauer <marceloknauer.dev@gmail.com>
2026-01-02 17:48:21 -0300

    Check words
    Write a Java program to implement a lambda expression to check if a list of strings contains a specific word.

---
+++ Lambda Expression/CheckWords/Main.java
@@ -0,0 +1,15 @@
+package Desafios.LambdaExpression.CheckWords;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> words = new ArrayList<>(List.of("Red", "Green", "Blue", "Orange", "Black"));
+
+        String searchColor = "Orange";
+
+        boolean checkColor = words.stream().anyMatch(w -> w.equalsIgnoreCase(searchColor));
+        System.out.println(checkColor ? searchColor+" is in the list" : searchColor+" is not in the list");
+    }
+}
