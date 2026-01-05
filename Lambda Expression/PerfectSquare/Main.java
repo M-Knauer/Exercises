7d6ddb217a6727f79ab434689e26dd3fc644a8ba
Marcelo Knauer <marceloknauer.dev@gmail.com>
2026-01-05 14:17:29 -0300

    Floor
    Write a Java program to implement a lambda expression that checks if a number is a perfect square using Math.sqrt and floor.

--- Lambda Expression/PerfectSquare/Main.java
+++ Lambda Expression/PerfectSquare/Main.java
@@ -6,5 +6,12 @@ public class Main {
     public static void main(String[] args) {
         Predicate<Integer> isPerfectSquare = n -> (Math.sqrt(n) - (int) Math.sqrt(n)) == 0;
         System.out.println(isPerfectSquare.test(10000));
+
+        isPerfectSquare = n -> {
+            int res = (int) Math.floor(Math.sqrt(n));
+            return res * res == n;
+        };
+
+        System.out.println(isPerfectSquare.test(35));
     }
 }
