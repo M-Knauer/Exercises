eeec58ef3585ec4a9185877c9b561542b815f99b
Marcelo Knauer <marceloknauer.dev@gmail.com>
2026-01-05 13:48:45 -0300

    Perfect square
    Write a Java program to implement a lambda expression to check if a given number is a perfect square.

---
+++ Lambda Expression/PerfectSquare/Main.java
@@ -0,0 +1,10 @@
+package Desafios.LambdaExpression.PerfectSquare;
+
+import java.util.function.Predicate;
+
+public class Main {
+    public static void main(String[] args) {
+        Predicate<Integer> isPerfectSquare = n -> (Math.sqrt(n) - (int) Math.sqrt(n)) == 0;
+        System.out.println(isPerfectSquare.test(10000));
+    }
+}
