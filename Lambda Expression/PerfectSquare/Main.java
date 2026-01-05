d372f5ed22f8cffa0dbc8f3524a140295483cba9
Marcelo Knauer <marceloknauer.dev@gmail.com>
2026-01-05 14:38:20 -0300

    Round
    Write a Java program to create a lambda that verifies the perfect square property by squaring the rounded square root.

--- Lambda Expression/PerfectSquare/Main.java
+++ Lambda Expression/PerfectSquare/Main.java
@@ -5,13 +5,21 @@ import java.util.function.Predicate;
 public class Main {
     public static void main(String[] args) {
         Predicate<Integer> isPerfectSquare = n -> (Math.sqrt(n) - (int) Math.sqrt(n)) == 0;
-        System.out.println(isPerfectSquare.test(10000));
+        int num = 10000;
+        System.out.println(num+" is a perfect square? "+isPerfectSquare.test(num));
 
         isPerfectSquare = n -> {
             int res = (int) Math.floor(Math.sqrt(n));
             return res * res == n;
         };
 
-        System.out.println(isPerfectSquare.test(35));
+        System.out.println(num+" is a perfect square using floor? "+isPerfectSquare.test(num));
+
+        isPerfectSquare = n -> {
+            long res = Math.round(Math.sqrt(n));
+            return res * res == n;
+        };
+
+        System.out.println(num+" is a perfect square after rounding? "+isPerfectSquare.test(num));
     }
 }
