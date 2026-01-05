f7e4d2af00904e55c934d166aedf0a8e2e2e88ea
Marcelo Knauer <marceloknauer.dev@gmail.com>
2026-01-05 17:40:21 -0300

    Distinct, sorting and skipping
    Write a Java program to implement a lambda expression to find the second largest and smallest element in an array.

---
+++ Lambda Expression/SecondLargAndSmall/Main.java
@@ -0,0 +1,18 @@
+package Desafios.LambdaExpression.SecondLargAndSmall;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> numbers = List.of(1, 1, 7, 18, 25, 21, 77, 39, 300, 101);
+
+        System.out.println("Getting the second largest and smallest numbers in the list: "+numbers);
+        int secondLargest = numbers.stream()
+                .distinct().sorted(Comparator.reverseOrder()).skip(1).findFirst().orElse(0);
+        int secondSmallest = numbers.stream().distinct().sorted().skip(1).findFirst().orElse(0);
+
+        System.out.println("Second largest: "+secondLargest);
+        System.out.println("Second smallest: "+secondSmallest);
+    }
+}
