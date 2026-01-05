35e00ee87f131756f62456fe15cb75cd4539969e
Marcelo Knauer <marceloknauer.dev@gmail.com>
2026-01-05 18:12:17 -0300

    filtering out the maximum value
    Write a Java program to create a lambda that identifies the second largest number in an array by filtering out the maximum value.

--- Lambda Expression/SecondLargAndSmall/Main.java
+++ Lambda Expression/SecondLargAndSmall/Main.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<Integer> numbers = List.of(1, 1, 7, 18, 25, 21, 77, 39, 300, 101);
+        List<Integer> numbers = List.of(1, 1, 7, 18, 25, 21, 77, 39, 300, 300, 101);
 
         System.out.println("Getting the second largest and smallest numbers in the list: "+numbers);
         int secondLargest = numbers.stream()
@@ -14,5 +14,21 @@ public class Main {
 
         System.out.println("Second largest: "+secondLargest);
         System.out.println("Second smallest: "+secondSmallest);
+
+        System.out.println("\n" +
+                "Getting the second number in the list by filtering out the maximum value: "+numbers);
+
+        secondLargest = numbers
+                .stream()
+                .filter(n ->
+                        n < numbers
+                                .stream()
+                                .mapToInt(Integer::intValue)
+                                .max().orElse(0))
+                .max(Integer::compareTo).orElse(0);
+
+        System.out.println("Second largest: "+secondLargest);
+
+
     }
 }
