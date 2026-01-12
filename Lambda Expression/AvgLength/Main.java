9d374a84be4312f10e0aa5c5c3f5d27856c009c5
Marcelo Knauer <marceloknauer.dev@gmail.com>
2026-01-12 20:29:03 -0300

    Filtering out
    Write a Java program to create a lambda that computes the average length of strings after filtering out empty entries.

--- Lambda Expression/AvgLength/Main.java
+++ Lambda Expression/AvgLength/Main.java
@@ -5,11 +5,16 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<String> colors = new ArrayList<>(List.of("Red", "Black", "White", "Orange", "Blue"));
+        List<String> colors = new ArrayList<>(List.of("Red", "Black", "White", "Orange", "Blue", "", "", "    "));
 
         double avg = colors.stream().mapToInt(String::length).average().orElse(0);
 
         System.out.println(colors);
+        System.out.println("Average length of strings: "+String.format("%.1f", avg));
+
+        System.out.println("\nFiltering out empty entries");
+        System.out.println(colors);
+        avg = colors.stream().filter(c -> !c.isBlank()).mapToInt(String::length).average().orElse(0);
         System.out.println("Average length of strings: "+String.format("%.1f", avg));
     }
 }
