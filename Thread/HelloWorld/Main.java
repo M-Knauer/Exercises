2fe016eb7d464ea7720900b9a4475ab2837f43a7
Marcelo Knauer <marceloknauer.dev@gmail.com>
2026-01-18 20:26:51 -0300

    Thread logs
    Write a Java program to create a thread that prints "Hello, World!" and logs its thread ID and priority.

--- Thread/HelloWorld/Main.java
+++ Thread/HelloWorld/Main.java
@@ -4,7 +4,13 @@ import Desafios.Thread.HelloWorld.Models.HelloWorldThread;
 
 public class Main {
     public static void main(String[] args) {
-        Thread thread = new Thread(() -> System.out.println("Hello, world!"));
+        Thread thread = new Thread(() ->{
+            System.out.printf("""
+                    Hello, world!
+                    Thread id: %d
+                    Thread priority: %d
+                    """, Thread.currentThread().getId(), Thread.currentThread().getPriority());
+        });
         thread.start();
     }
 }
--- Thread/HelloWorld/Models/HelloWorldThread.java
+++ Thread/HelloWorld/Models/HelloWorldThread.java
@@ -5,5 +5,6 @@ public class HelloWorldThread implements Runnable {
     @Override
     public void run() {
         System.out.println("Hello, world!");
+        System.out.println(Thread.currentThread());
     }
 }
