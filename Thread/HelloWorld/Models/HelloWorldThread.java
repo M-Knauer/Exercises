a3eb6df03718381c91737e715f949c1c212dd648
Marcelo Knauer <marceloknauer.dev@gmail.com>
2026-01-18 18:40:55 -0300

    Thread.sleep
    Write a Java program to create a thread that prints "Hello, World!" repeatedly for 10 iterations with a one-second delay using Thread.sleep().

--- Thread/HelloWorld/Models/HelloWorldThread.java
+++ Thread/HelloWorld/Models/HelloWorldThread.java
@@ -3,6 +3,14 @@ package Desafios.Thread.HelloWorld.Models;
 public class HelloWorldThread implements Runnable {
     @Override
     public void run() {
-        System.out.println("Hello, world!");
+        try {
+            for (int i = 0; i < 10; i++) {
+                System.out.println("Hello, world!");
+                Thread.sleep(1000);
+            }
+
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
