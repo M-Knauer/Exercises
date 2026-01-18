abd44d50f9b4671949e2c74567c487b1d750d8b5
Marcelo Knauer <marceloknauer.dev@gmail.com>
2026-01-18 19:07:27 -0300

    Lambda
    Write a Java program to create a thread using a lambda expression that prints "Hello, World!" and then terminates.

--- Thread/HelloWorld/Main.java
+++ Thread/HelloWorld/Main.java
@@ -4,7 +4,7 @@ import Desafios.Thread.HelloWorld.Models.HelloWorldThread;
 
 public class Main {
     public static void main(String[] args) {
-        HelloWorldThread hwt = new HelloWorldThread("HelloThread");
-        hwt.start();
+        Thread thread = new Thread(() -> System.out.println("Hello, world!"));
+        thread.start();
     }
 }
--- Thread/HelloWorld/Models/HelloWorldThread.java
+++ Thread/HelloWorld/Models/HelloWorldThread.java
@@ -1,11 +1,9 @@
 package Desafios.Thread.HelloWorld.Models;
 
-public class HelloWorldThread extends Thread {
-    public HelloWorldThread(String name) {
-        super(name);
-    }
+public class HelloWorldThread implements Runnable {
+
     @Override
     public void run() {
-        System.out.println("Hello, world and "+super.getName()+"!");
+        System.out.println("Hello, world!");
     }
 }
