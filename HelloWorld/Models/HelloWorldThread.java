39ba317220228f73e77542073c358ce28ba3f3fb
Marcelo Knauer <marceloknauer.dev@gmail.com>
2026-01-18 18:29:36 -0300

    Delete HelloWorld directory

--- HelloWorld/Models/HelloWorldThread.java
+++
@@ -1,8 +0,0 @@
-package Desafios.Thread.HelloWorld.Models;
-
-public class HelloWorldThread implements Runnable {
-    @Override
-    public void run() {
-        System.out.println("Hello, world!");
-    }
-}
