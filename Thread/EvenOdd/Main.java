1be7d3c4e5a036bfc18948f996d75a57540beef3
Marcelo Knauer <marceloknauer.dev@gmail.com>
2026-01-21 15:54:28 -0300

    1 to 50
    Write a Java program to create two threads where one prints even numbers and the other prints odd numbers from 1 to 50 concurrently with proper synchronization.

--- Thread/EvenOdd/Main.java
+++ Thread/EvenOdd/Main.java
@@ -1,42 +1,41 @@
 package Desafios.Thread.EvenOdd;
 
-import java.util.stream.IntStream;
-
 public class Main {
-    private static final Object lock = new Object();
-    private static int status = 2;
+    private static final Object LOCK = new Object();
+    private static int status = 1;
+    private static final int LIMIT = 50;
     public static void main(String[] args) {
 
         Thread even = new Thread(() -> {
-            for (int i = 0; i <= 20; i+=2) {
-                synchronized (lock) {
+            for (int i = 2; i <= LIMIT; i+=2) {
+                synchronized (LOCK) {
                     while (status != 2) {
                         try {
-                            lock.wait();
+                            LOCK.wait();
                         } catch (InterruptedException e) {
                             throw new RuntimeException(e);
                         }
                     }
                     status = 1;
                     System.out.println("Even: "+i);
-                    lock.notify();
+                    LOCK.notify();
                 }
             }
         });
 
         Thread odd = new Thread(() -> {
-            for (int i = 1; i < 20; i+=2) {
-                synchronized (lock) {
+            for (int i = 1; i < LIMIT; i+=2) {
+                synchronized (LOCK) {
                     while (status != 1) {
                         try {
-                            lock.wait();
+                            LOCK.wait();
                         } catch (InterruptedException e) {
                             throw new RuntimeException(e);
                         }
                     }
                     status = 2;
                     System.out.println("Odd: "+i);
-                    lock.notify();
+                    LOCK.notify();
                 }
             }
         });
