63e302ec833d2eb59c73ba097b77897b91ad1da8
Marcelo Knauer <marceloknauer.dev@gmail.com>
2026-01-21 14:52:19 -0300

    Wait & notify

--- Thread/EvenOdd/Main.java
+++ Thread/EvenOdd/Main.java
@@ -3,16 +3,42 @@ package Desafios.Thread.EvenOdd;
 import java.util.stream.IntStream;
 
 public class Main {
+    private static final Object lock = new Object();
+    private static int status = 2;
     public static void main(String[] args) {
 
         Thread even = new Thread(() -> {
-            IntStream.rangeClosed(1, 20).filter(n -> n % 2 == 0)
-                    .forEach(n -> System.out.println("Even: "+n));
+            for (int i = 0; i <= 20; i+=2) {
+                synchronized (lock) {
+                    while (status != 2) {
+                        try {
+                            lock.wait();
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                    status = 1;
+                    System.out.println("Even: "+i);
+                    lock.notify();
+                }
+            }
         });
 
         Thread odd = new Thread(() -> {
-            IntStream.rangeClosed(1, 20).filter(n -> n % 2 == 1)
-                    .forEach(n -> System.out.println("Odd: "+n));
+            for (int i = 1; i < 20; i+=2) {
+                synchronized (lock) {
+                    while (status != 1) {
+                        try {
+                            lock.wait();
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                    status = 2;
+                    System.out.println("Odd: "+i);
+                    lock.notify();
+                }
+            }
         });
 
         even.start();
