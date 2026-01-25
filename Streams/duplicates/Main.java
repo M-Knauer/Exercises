c493c3a77356305e51c532f2a37f8e2f0cefcd25
Marcelo Knauer <marceloknauer.dev@gmail.com>
2026-01-24 23:31:50 -0300

    Distinct
    Write a Java program to remove all duplicate elements from a list using streams.

---
+++ Streams/duplicates/Main.java
@@ -0,0 +1,14 @@
+package Desafios.Streams.duplicates;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> numbers = new ArrayList<>(List.of(10, 23, 22, 23, 24, 24, 33, 15, 26, 15));
+        System.out.println("Original list: "+numbers);
+        List<Integer> nonDuplicates =  new ArrayList<>(numbers.stream().distinct().toList());
+        System.out.println("Non duplicate numbers list: "+nonDuplicates);
+
+    }
+}
